d43d675be5a38e2fc21037f3bd1f6f63a51e6037
AzureAD\LucasNkoana <lucasn@dotcomsoftware.co.za>
2022-07-15 12:02:53 +0200

    Added handlebars template engine

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 const express = require('express');
 const app = express();
-
+const members = require('./members');
+const { engine } = require('express-handlebars');
 //NodeJS module to deal with file paths
 const path = require('path');
 
+//Handlebars middleware
+//app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
 const logger = require('./middleware/logger');
 //Body parser middleware
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
 //Init middleware
 app.use(logger);
+
+//Home page route using handlebars
+app.get('/', (req, res) =>
+  res.render('index', {
+    title: 'Testing',
+    members: members,
+  })
+);
 
 //Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
